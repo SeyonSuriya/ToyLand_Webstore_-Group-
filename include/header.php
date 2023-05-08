c467db7d5db68e72069209c4b00666587b416ff0
Seyon <suriyaseyon6@gmail.com>
2023-05-08 20:58:22 +0530

    header styles added

--- include/header.php
+++ include/header.php
@@ -1,3 +1,9 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="utility.css">
+<link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 <div class="container-menu">
             <!-- Topbar -->
             <div class="top-bar">
