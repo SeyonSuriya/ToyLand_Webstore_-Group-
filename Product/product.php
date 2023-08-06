24ca6cb95da24aba6acd065b4cc5857481e78109
Hiransanjeewa <hiransanjeewaa@gmail.com>
2023-08-06 21:05:18 +0530

    Products image added

--- Product/product.php
+++ Product/product.php
@@ -16,11 +16,27 @@ $_SESSION['productID'] = $productID;
     <meta charset="UTF-8">
     <script src="productJavascript.js" > 
     </script>
-  <link rel="stylesheet" href="../mainpage/style.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="../mainpage/style.css"/>
+    <link rel="stylesheet" href="../mainpage/utility.css"/>
+    <link rel="stylesheet" href="../mainpage/footer.css"/>
     
-
+    <script src="../mainpage/app.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
+        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
     <script>
-                function changeImage(x) {
+        function changeImage(x) {
     switch (x) {
         case 2:
             document.getElementById('image0').src="<?php echo $folder ?>/2.webp";
@@ -84,7 +100,10 @@ document.getElementById('shippingCost').innerHTML=shipping.toFixed(2);
 ?>
 
 <body>
-
+    <?php                     
+        $page = 'home'; 
+        include '../mainpage/header.php';                 
+    ?>
     <br><br><br>
     <!--
         Header comes here
@@ -98,7 +117,8 @@ document.getElementById('shippingCost').innerHTML=shipping.toFixed(2);
             <img  id="image0" class="mainimage" src="<?php echo $folder ?>/1.webp" alt="image1" >
 
             </span>
-            </div>
+
+        </div>
         <div class="secondaryimagediv">
         <img onmouseenter="changeImage(1)" class="secondaryimage" id="image1" src="<?php echo $folder ?>/1.webp" alt="image1">
         <img onmouseenter="changeImage(2)" class="secondaryimage" id="image2" src="<?php echo $folder ?>/2.webp" alt="image2">
@@ -106,8 +126,6 @@ document.getElementById('shippingCost').innerHTML=shipping.toFixed(2);
         <img onmouseenter="changeImage(4)" class="secondaryimage" id="image4" src="<?php echo $folder ?>/4.webp" alt="image4">
         <img onmouseenter="changeImage(5)" class="secondaryimage" id="image5" src="<?php echo $folder ?>/5.webp" alt="image5">
         </div>
-
-    
     </div>
     <?php
 
